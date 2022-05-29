240011ea16038ee5bffa5e3b183482cba1269b4f
Amy Stanley <98853049+ameliasheppy@users.noreply.github.com>
2022-05-29 16:51:30 -0400

    Here I have been creating the function to get the weather info
    I used the OpenWeather API doc along with Matt Delac to select the weather info that I wanted to use and add it into my project. Next we will be working on getting the current date and time.

--- guess.js
+++ guess.js
@@ -28,3 +28,28 @@ function toDoList(event) {
 }
 let launchList = document.querySelector("#launch-list");
 launchList.addEventListener("click", toDoList);
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temp");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  cityElement.innerHTML = response.data.name;
+
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  
+}
+
+let apiKey = "0aa51f2ae72d62c67ab574237edb123f";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
