0a9abc9592e49ac274722fa4348e5ccbc324e884
Amy Stanley <98853049+ameliasheppy@users.noreply.github.com>
2022-05-29 21:15:32 -0400

    Adding the search engine
    Here I added on a form to use as a search engine for the user to find the current weather conditions in a city of their choosing. This involved changes to the HTML and the JS.

--- guess.js
+++ guess.js
@@ -80,8 +80,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "0aa51f2ae72d62c67ab574237edb123f";
-let city = "Los Angeles";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+function search(city) {
+  let apiKey = "0aa51f2ae72d62c67ab574237edb123f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
